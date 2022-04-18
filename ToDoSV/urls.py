e591814015cb63c06b03c4737544336db9cbede0
Thang-Quoc <winofwin292@gmail.com>
2022-04-18 12:29:29 +0700

    First Commit

--- ToDoSV/urls.py
+++ ToDoSV/urls.py
@@ -23,5 +23,5 @@ router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
